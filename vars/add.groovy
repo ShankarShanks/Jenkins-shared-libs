44b75150c2cff71af8eeba3eff3b2804082fe852
Shankar shanks <118338969+ShankarShanks@users.noreply.github.com>
2022-12-20 19:57:12 +0530

    Update add.groovy

--- vars/add.groovy
+++ vars/add.groovy
@@ -1,4 +1,8 @@
- script{
+parameters {
+  string defaultValue: '10', description: 'Enter value for x', name: 'VarOne'
+  string defaultValue: '20', description: 'Enter value for y', name: 'VarTwo'
+}
+script{
   def varOne = params.varOne as int
   def varTwo = params.varTwo as int
   def result = varOne + varTwo
