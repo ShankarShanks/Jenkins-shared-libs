02e1b4657075eb27323b49a3a72735d98c9d32be
Shankar shanks <118338969+ShankarShanks@users.noreply.github.com>
2022-12-21 15:19:28 +0530

    Update add.groovy

--- vars/add.groovy
+++ vars/add.groovy
@@ -1,10 +1,4 @@
-parameters {
-  string defaultValue: '10', description: 'Enter value for x', name: 'VarOne'
-  string defaultValue: '20', description: 'Enter value for y', name: 'VarTwo'
+def call(x,y) {
+  def result = x + y
+  echo "Result is $result"
 }
-script{
-  def varOne = params.varOne as int
-  def varTwo = params.varTwo as int
-  def result = varOne + varTwo
-    echo "Result is ${result}" 
- }    
